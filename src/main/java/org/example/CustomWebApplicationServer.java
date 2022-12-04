a9f2067b22ee39836cfb60a7dc22ca0271ffd5f7
Yeeun Kim <maemae2@naver.com>
2022-12-04 23:36:58 +0900

    10P. Part1 - Ch04 - 04. 계산기 웹 프로그램 구현하기 - Step 3 : Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,10 +9,15 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -30,7 +35,12 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
                 // Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                // ㄴ 문제점 1 : 성능 하락
+                // ㄴ 문제점 2 : 동시 접속자 수가 많아질 경우, 많은 쓰레드가 생성되어 최악의 경우 서버의 리소스 한계로 인해 서버가 다운될 수 있음
+
+                // Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                executorService.execute(new ClientRequestHandler(clientSocket));
+//                new Thread(new ClientRequestHandler(clientSocket)).start();
             }
         }
     }
